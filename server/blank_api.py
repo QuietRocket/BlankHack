64f1bc15053f703e4d357d7f8d9743db25d4043d
Angel <mendoza.angel562@gmail.com>
2020-02-01 17:51:29 -0800

    Updated blank_api.py

--- server/blank_api.py
+++ server/blank_api.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/meep", methods=['POST'])
+def other():
+    print(request.get_json())
+    return jsonify({'you sent': request.get_json()})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
